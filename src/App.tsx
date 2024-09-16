4b8883dc494423e3a1521d66ac229a8f79d1f740
JanuDev <darkober12@gmail.com>
2024-09-16 18:33:46 -0400

    added font "Onest"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "@fontsource-variable/onest";
 
 function App() {
 
   return (
     <>
-    
+    <div>
+      Hola
+    </div>
     </>
   )
 }
