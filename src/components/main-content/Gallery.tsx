d81cefdba9e7062664ac91cf32a34ce69853dc55
JanuDev <darkober12@gmail.com>
2024-09-25 12:11:14 -0400

    add ui edit button

--- src/components/main-content/Gallery.tsx
+++ src/components/main-content/Gallery.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel,  DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { useCollectionStore } from "@/store/collections-store";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { Input } from "../ui/input";
 
 interface MasonryCardProps {
   data: {
@@ -81,7 +84,21 @@ function MasonryCard({ data: { id, name, src, lastModified, artist, tags } }: Ma
       <div className="bg-black/0 hover:bg-black/50 absolute w-full h-full top-0 left-0 opacity-0 hover:opacity-100 p-3 flex flex-col justify-between transition-opacity duration-200 ease-in-out pointer-events-none group-hover:pointer-events-auto group-focus-within:pointer-events-auto ">
         <DropdownMenu>
           <DropdownMenuTrigger className="flex justify-center w-28 h-6 mx-auto gap-1 items-center">
-            <span className="text-sm">Collection</span>
+            {selectedCollections.length > 0 ? (
+              <span className="text-sm">
+                {selectedCollections
+                  .map((collectionId) => {
+                    const collection = collections.find(
+                      (col) => col.id === collectionId
+                    );
+                    return collection?.name ?? ""; // Muestra el nombre de la colección si está
+                  })
+                  .slice(-1)
+                  .join(", ")}
+              </span>
+            ) : (
+              <span className="text-sm"></span> // Texto por defecto si no pertenece a ninguna colección
+            )}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="15"
@@ -124,29 +141,75 @@ function MasonryCard({ data: { id, name, src, lastModified, artist, tags } }: Ma
           </DropdownMenuContent>
         </DropdownMenu>
 
+        {/* EDIT IMAGE */}
+
         <div className="flex justify-between items-end">
           <p className="text-xs text-[--color-light-tertiary]">@{artist}</p>
-          <Button
-            variant="outline"
-            className="bg-transparent border-[--color-light-tertiary] size-8 p-1 rounded-3xl"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-              <path d="M13.5 6.5l4 4" />
-            </svg>
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                variant="outline"
+                className="bg-transparent border-[--color-light-tertiary] size-8 p-1 rounded-3xl"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                  <path d="M13.5 6.5l4 4" />
+                </svg>
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[550px]">
+              <DialogHeader>
+                <DialogTitle>Edit Image</DialogTitle>
+              </DialogHeader>
+              <div className="flex gap-7">
+                <div className="flex flex-col w-[50%] gap-3">
+                  <div className="flex flex-col gap-2 justify-between">
+                    <span className="text-sm">Name</span>
+                    <Input />
+                  </div>
+
+                  <div className="flex flex-col gap-2 justify-between">
+                    <span className="text-sm">Tags</span>
+                    <Input />
+                  </div>
+
+                  <div className="flex flex-col gap-2 justify-between">
+                    <span className="text-sm">Artist</span>
+                    <Select>
+                      <SelectTrigger className="min-w-[180px]">
+                        <SelectValue placeholder="Theme" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="light">Light</SelectItem>
+                        <SelectItem value="dark">Dark</SelectItem>
+                        <SelectItem value="system">System</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <img src={src} alt="as" className="rounded-xl" />
+                </div>
+              </div>
+
+              <DialogFooter className="justify-between mt-6">
+                <Button variant="destructive">Delete</Button>
+                <Button variant="white"> Save Changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
@@ -176,7 +239,7 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
       render={MasonryCard}
       columnGutter={16}
       columnWidth={172}
-      overscanBy={5}
+      overscanBy={20}
       maxColumnCount={4}
     ></Masonry>
   );
--- src/pages/Collections/Collections.tsx
+++ src/pages/Collections/Collections.tsx
@@ -3,6 +3,8 @@ import Layout from '@/components/main-content/Layout';
 import { useCollectionStore } from '@/store/collections-store';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
 
 function Collections() {
   const navigate = useNavigate();
@@ -82,6 +84,8 @@ function Collections() {
       }
       pageHeader={rightHeader}
     >
+
+      
       <div className="py-2 flex gap-x-8 gap-y-16 flex-wrap">
         {collections.length === 0 ? (
           <p>Not Collection created.</p>
@@ -95,7 +99,7 @@ function Collections() {
             return (
               <div
                 key={collection.id}
-                className="max-w-52 max-h-36 cursor-pointer group"
+                className="max-w-52 max-h-36 cursor-pointer group relative"
                 onClick={() => navigate(`/collections/${collection.id}`)}
               >
                 <div className="flex gap-[2px] w-full h-full rounded-xl overflow-hidden relative">
@@ -118,8 +122,67 @@ function Collections() {
                     ))}
                   </div>
 
-                  {/* background hover */}
-                  <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                  {/* hover */}
+                  <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <div className="absolute bottom-0 right-0 p-3">
+                      <Dialog>
+                        <DialogTrigger
+                          asChild
+                          className="z-50"
+                          onClick={(e) => e.stopPropagation()}
+                        >
+                          <Button
+                            variant="outline"
+                            className="bg-transparent border-[--color-light-tertiary] size-8 p-1 rounded-3xl"
+                            onClick={(e) => e.stopPropagation()}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="18"
+                              height="18"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
+                            >
+                              <path
+                                stroke="none"
+                                d="M0 0h24v24H0z"
+                                fill="none"
+                              />
+                              <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                              <path d="M13.5 6.5l4 4" />
+                            </svg>
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent
+                          className="max-w-[350px]"
+                          onClick={(e) => e.stopPropagation()}
+                        >
+                          <DialogHeader>
+                            <DialogTitle>Edit Collection</DialogTitle>
+                          </DialogHeader>
+
+                          <div className="flex flex-col w-full gap-3">
+                            <div className="flex flex-col gap-2 justify-between">
+                              <span className="text-sm">Name</span>
+                              <Input />
+                            </div>
+                          </div>
+
+                          <DialogFooter className="justify-between mt-6">
+                            <Button variant="destructive">
+                              Delete Collection
+                            </Button>
+                            <Button variant="white"> Save Changes</Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+                    </div>
+                  </div>
                 </div>
 
                 <div className="my-2 mx-1">
